a35dbff9827c7a05d755ecae597373fc881f2fb5
rob1694 <robertb1694@gmail.com>
2021-03-19 11:53:01 -0400

    had trouble with without delete key

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {name: "" ,streetAddress: ""}
+
+
+function updateEmployeeWithKeyAndValue(employee) {
+    const newEmployee = {...employee};
+    newEmployee.streetAddress = "11 Broadway";
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey () {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    const newEmployee = employee;
+    delete newEmployee.name;
+    return newEmployee;
+}
